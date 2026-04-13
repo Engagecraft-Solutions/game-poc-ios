2524f1caf703fb5d7c3fdbe8b2d7f84c97c1f0af
Aurimas Petrevicius <aurimas@notnot.xyz>
2026-04-13 17:20:02 +0300

    Initial Commit

---
+++ dtfantasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dtfantasy
+//
+//  Created by Aurimas Petrevicius on 13/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
